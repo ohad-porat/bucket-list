a5ef7525539a54a7ddde9e069c545b32b45425fe
Ohad Porat <ohadp23@gmail.com>
2021-03-17 19:53:23 -0400

    responsive design for mobile

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -6,19 +6,24 @@ import SignOutButton from "../authentication/SignOutButton"
 const TopBar = ({ user }) => {
   const unauthenticatedRightNavBar = [
     <li key="sign-in" id="sign-in" className="top-bar-user">
-      <Link to="/user-sessions/new"className="top-bar-link">Sign In</Link>
+      <Link to="/user-sessions/new" className="top-bar-link dropdown-text">
+        Sign In
+      </Link>
     </li>,
     <li key="sign-up" className="top-bar-user">
-      <Link to="/users/new" className="button sign-up-button">Sign Up</Link>
+      <Link to="/users/new" className="button sign-up-button dropdown-text">
+        Sign Up
+      </Link>
     </li>,
   ]
 
   let username = ""
-if (user) {
-  username = user.userName
-}
+  if (user) {
+    username = user.userName
+  }
   const authenticatedRightNavBar = [
-    <li key="hello-user" className="hello-user top-bar-user">Hello, {username}
+    <li key="hello-user" className="hello-user top-bar-user dropdown-text">
+      Hello, {username}
     </li>,
     <li key="sign-out" id="sign-out" className="top-bar-user">
       <SignOutButton />
@@ -29,30 +34,54 @@ if (user) {
   if (user) {
     authenticatedLeftNavbar = (
       <li>
-        <Link to={`/${user.id}/my-tables`} className="top-bar-link" id="my-tables">My Tables</Link>
+        <Link
+          to={`/${user.id}/my-tables`}
+          className="top-bar-link"
+          id="my-tables"
+        >
+          My Tables
+        </Link>
       </li>
     )
   }
 
   const unauthenticatedLeftNavBar = ""
+  const toggleTopBar = () => {
+    let toggle = document.getElementById("rightTopBar")
+    if (toggle.className === "dropdown-close") {
+      toggle.className = "dropdown-open"
+    } else {
+      toggle.className = "dropdown-close"
+    }
+  }
 
   return (
     <div className="top-bar">
       <Link to="/" className="top-bar-logo-link">
-        <img src="https://i.ibb.co/r3DCrp4/logo.png" alt="Website Logo" className="top-bar-logo" />
+        <img
+          src="https://i.ibb.co/r3DCrp4/logo.png"
+          alt="Website Logo"
+          className="top-bar-logo"
+        />
       </Link>
       <div className="top-bar-left">
         <ul className="menu">
           <li>
-            <Link to="/all-tables" className="top-bar-link" id="all-tables">All Tables</Link>
+            <Link to="/all-tables" className="top-bar-link" id="all-tables">
+              All Tables
+            </Link>
           </li>
           {user ? authenticatedLeftNavbar : unauthenticatedLeftNavBar}
         </ul>
       </div>
       <div className="top-bar-right">
         <ul className="menu">
-          {user ? authenticatedRightNavBar : unauthenticatedRightNavBar}
-          <li><i className="fas fa-bars" /></li>
+          <li onClick={toggleTopBar}>
+            <i className="fas fa-bars" />
+          </li>
+          <div className="dropdown-close" id="rightTopBar">
+            {user ? authenticatedRightNavBar : unauthenticatedRightNavBar}
+          </div>
         </ul>
       </div>
     </div>
