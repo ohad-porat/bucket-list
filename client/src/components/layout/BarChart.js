89d59f3db37adb60e77f3b82752a21be4eb85ee3
ohad-porat <ohadp23@gmail.com>
2022-05-02 00:36:42 -0400

    fix charts

--- client/src/components/layout/BarChart.js
+++ client/src/components/layout/BarChart.js
@@ -1,19 +1,22 @@
 import React from "react"
 import Chart from "react-google-charts"
 
-const BarChart = ({selectedPlayers, selectedStats}) => {
-  let data = [["Stats"]]
-  selectedStats.forEach((stat) => {
-    data[0].push(stat.abbreviation)
-  });
-  
-    selectedPlayers.forEach((player, stats = selectedStats) => {
-      let playerArray =[`${player.player.first_name} ${player.player.last_name}, ${player.season}`]
-      for (let i = 0; i < selectedStats.length; i++) {
-        playerArray.push(player[selectedStats[i].value])
-      }
-      data.push(playerArray)
-    });
+const BarChart = ({ selectedPlayers, selectedStats }) => {
+  let data = [["Players"]]
+
+  selectedPlayers.forEach((player) => {
+    data[0].push(
+      `${player.player.first_name} ${player.player.last_name}, ${player.season}`
+    )
+  })
+
+  selectedStats.forEach((stat, players = selectedPlayers) => {
+    let statArray = [stat.abbreviation]
+    for (let i = 0; i < selectedPlayers.length; i++) {
+      statArray.push(selectedPlayers[i][stat.value])
+    }
+    data.push(statArray)
+  })
 
   return (
     <div className="chart medium-12 cell">
@@ -21,26 +24,32 @@ const BarChart = ({selectedPlayers, selectedStats}) => {
         width="980px"
         height="392px"
         chartType="BarChart"
-        loader={<img src="https://i.ibb.co/X2V5Fb0/loader.gif" alt="loading" className="chart-loader" />}
+        loader={
+          <img
+            src="https://i.ibb.co/X2V5Fb0/loader.gif"
+            alt="loading"
+            className="chart-loader"
+          />
+        }
         data={data}
         options={{
-          backgroundColor: { fill:'transparent' },
-          textStyle:{color: '#eae7e2'},
+          backgroundColor: { fill: "transparent" },
+          textStyle: { color: "#eae7e2" },
           chartArea: { width: "65%", height: "75%" },
           hAxis: {
-            textStyle:{color: '#eae7e2'},
-            titleTextStyle:{color: '#eae7e2'},
+            textStyle: { color: "#eae7e2" },
+            titleTextStyle: { color: "#eae7e2" },
             title: "Stats",
             minValue: 0,
           },
           vAxis: {
-            textStyle:{color: '#eae7e2'},
-            titleTextStyle:{color: '#eae7e2'},
+            textStyle: { color: "#eae7e2" },
+            titleTextStyle: { color: "#eae7e2" },
             title: "Players",
           },
           legend: {
-            textStyle:{color: '#eae7e2'},
-          }
+            textStyle: { color: "#eae7e2" },
+          },
         }}
         legendToggle
       />
